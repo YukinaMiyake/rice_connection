6e6f18829b919c9ada211580091be6528590028a
EC2 Default User <ec2-user@ip-172-31-37-86.ap-northeast-1.compute.internal>
2024-10-29 22:05:47 +0000

    admin側 会員情報、ジャンル修正(レビュー後修正)

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,8 @@ class CartItem < ApplicationRecord
   belongs_to :consumer
   belongs_to :item
   
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+  
   def subtotal 
     item.with_tax_price * amount
   end
